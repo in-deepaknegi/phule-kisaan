e65b611f6d367190a15aac6ac17cb22a4f260e11
in-deepaknegi <negideepak.in@gmail.com>
2025-01-31 11:49:20 +0530

    performance improvements

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        optimizePackageImports: ["@remixicon/react"],
+    },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
